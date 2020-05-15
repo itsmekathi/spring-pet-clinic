f6ea65b6861b871083ee8455d82c4d28892da9d2
kathiravan B <itsmekathi92@gmail.com>
2020-05-15 13:04:38 +0530

    refactoring to common crud interface.closes #11

--- pet-clinic-data/src/main/java/com/aayud/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/aayud/sfgpetclinic/services/OwnerService.java
@@ -2,11 +2,6 @@ package com.aayud.sfgpetclinic.services;
 
 import com.aayud.sfgpetclinic.models.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
