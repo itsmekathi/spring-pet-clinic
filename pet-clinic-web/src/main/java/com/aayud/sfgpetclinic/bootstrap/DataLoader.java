ca58804e147c9371a8c75fcc3e3dfb606fa3fe04
kathiravan B <itsmekathi92@gmail.com>
2020-05-23 21:25:41 +0530

    Loaded PetType data on bootstrap. closes #32

--- pet-clinic-web/src/main/java/com/aayud/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/aayud/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,11 @@
 package com.aayud.sfgpetclinic.bootstrap;
 
 import com.aayud.sfgpetclinic.models.Owner;
+import com.aayud.sfgpetclinic.models.PetType;
 import com.aayud.sfgpetclinic.models.Vet;
 import com.aayud.sfgpetclinic.services.OwnerService;
+import com.aayud.sfgpetclinic.services.PetService;
+import com.aayud.sfgpetclinic.services.PetTypeService;
 import com.aayud.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +15,26 @@ public class DataLoader implements CommandLineRunner {
 
     private  final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType =  petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType =  petTypeService.save(cat);
+        System.out.println("Loaded Pet Types");
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Kathiravan");
         owner1.setLastName("Balasubramanian");
