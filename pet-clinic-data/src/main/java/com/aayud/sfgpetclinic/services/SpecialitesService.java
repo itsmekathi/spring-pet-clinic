0e4b6fd0f2d4829dcd47b17113b69fffcd390358
kathiravan B <itsmekathi92@gmail.com>
2020-05-23 22:43:40 +0530

    Enhanced owner model with pets. closes #33

---
+++ pet-clinic-data/src/main/java/com/aayud/sfgpetclinic/services/SpecialitesService.java
@@ -0,0 +1,6 @@
+package com.aayud.sfgpetclinic.services;
+
+import com.aayud.sfgpetclinic.models.Speciality;
+
+public interface SpecialitesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/aayud/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package com.aayud.sfgpetclinic.services.map;
+
+import com.aayud.sfgpetclinic.models.Speciality;
+import com.aayud.sfgpetclinic.services.SpecialitesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return  super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
