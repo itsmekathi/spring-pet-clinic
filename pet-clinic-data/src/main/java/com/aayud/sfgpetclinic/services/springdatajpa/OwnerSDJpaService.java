f7f827ab95853368161e0dd03ea1f1495f5f85c1
kathiravan B <itsmekathi92@gmail.com>
2020-05-28 20:18:51 +0530

    Created Vet JPA service. closes #41

--- pet-clinic-data/src/main/java/com/aayud/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/com/aayud/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -5,10 +5,14 @@ import com.aayud.sfgpetclinic.repositories.OwnerRepository;
 import com.aayud.sfgpetclinic.repositories.PetRepository;
 import com.aayud.sfgpetclinic.repositories.PetTypeRepository;
 import com.aayud.sfgpetclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service()
+@Profile("springdatajpa")
 public class OwnerSDJpaService implements OwnerService {
     private final OwnerRepository ownerRepository;
     private final PetRepository petRepository;
